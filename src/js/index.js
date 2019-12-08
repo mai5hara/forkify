6cb2d7e28d091df2745105b634ea3f7e20e6b377
maigohara <maigohara-1103@outlook.jp>
2019-12-07 23:51:42 -0800

    add files

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+import Search from './models/Search';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //TODO
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = Search(query);
+
+        // 3) Prepare UI for result
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+   e.preventDefault();
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
